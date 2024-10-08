bbf9ca34d0b524284cb59ed2b80be62281408868
David Ramirez <daver3754@gmail.com>
2024-10-08 14:13:11 -0500

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -3,3 +3,4 @@
 first_name = input("Insert your first name: ")
 last_name = input("Insert your last name: ")
 print("hello",first_name,last_name,"and welcome to Python!")
+#The program asks for the users first and last name and greets them
