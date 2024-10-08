9f559b2ee38e30defb57765acd1f962eafe3fdb8
David Ramirez <daver3754@gmail.com>
2024-10-08 14:11:47 -0500

    Add files via upload

---
+++ Prob2.py
@@ -0,0 +1,5 @@
+#David Ramirez
+#10/8/2024 
+first_name = input("Insert your first name: ")
+last_name = input("Insert your last name: ")
+print("hello",first_name,last_name,"and welcome to Python!")
