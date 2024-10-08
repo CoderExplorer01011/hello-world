be1cbfff8ea9248b3252a3cbca75e9d0acc0493e
David Ramirez <daver3754@gmail.com>
2024-10-08 17:01:00 -0500

    Delete Prob2.py

--- Prob2.py
+++
@@ -1,6 +0,0 @@
-#David Ramirez
-#10/8/2024 
-first_name = input("Insert your first name: ")
-last_name = input("Insert your last name: ")
-print("hello",first_name,last_name,"and welcome to Python!")
-#The program asks for the users first and last name and greets them
